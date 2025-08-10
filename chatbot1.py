1d6655f4066aaf62f7004dafd277e97117dc3e23
patuz008 <32542978+patuz008@users.noreply.github.com>
2025-08-10 19:58:14 +0100

    Update chatbot1.py

--- chatbot1.py
+++ chatbot1.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
@@ -83,3 +84,4 @@ if st.button("Clear Conversation"):
     st.session_state.context = ""
 
     st.rerun()
+
